73dc2ada9d1e9143d8a7f93b5538a036b834b216
LM-Codespace <tripo54n0n@gmail.com>
2025-04-11 14:22:35 +0100

    Update models.py

--- models.py
+++ models.py
@@ -18,8 +18,12 @@ class Host(db.Model):
     resolved_hostname = db.Column(db.Text, nullable=True)
     location = db.Column(db.Text, nullable=True)
 
+    def __repr__(self):
+        return f'<Host {self.ip_address}>'
+
 class Proxies(db.Model):
     __tablename__ = 'proxies'
+    
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(100), nullable=False)
     port = db.Column(db.Integer, nullable=False)
@@ -31,10 +35,11 @@ class Proxies(db.Model):
 
 class Scan(db.Model):
     __tablename__ = 'scan'
+    
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    status = db.Column(db.String(50))
-    scan_type = db.Column(db.String(50))
+    status = db.Column(db.String(50), nullable=False)  # Ensures status is always defined
+    scan_type = db.Column(db.String(50), nullable=False)  # Scan type should also always be defined
     
     # Column for storing the target IP address for the scan
     ip_address = db.Column(db.String(50), nullable=False)
@@ -45,6 +50,6 @@ class Scan(db.Model):
 
     # Add this column to store the results of the scan (e.g., open ports, hostnames, etc.)
     results = db.Column(db.Text, nullable=True)  # Store scan results (e.g., open ports, hostnames, etc.)
-    
+
     def __repr__(self):
         return f'<Scan {self.id} - {self.scan_type}>'
