8eb74e99b4d87290e3db244521b870c0607cc65b
LM-Codespace <tripo54n0n@gmail.com>
2025-04-11 11:09:32 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize SQLAlchemy object in models (we'll initialize it later in app.py)
@@ -7,7 +6,7 @@ db = SQLAlchemy()
 class Host(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    ip = db.Column(db.String(100), nullable=False)
+    ip_address = db.Column(db.String(100), nullable=False)  # Changed from 'ip' to 'ip_address'
     resolved_hostname = db.Column(db.String(100), nullable=True)
     open_ports = db.Column(db.String(100), nullable=True)
     location = db.Column(db.String(100), nullable=True)
@@ -17,14 +16,13 @@ class Host(db.Model):
 
 class Proxies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ip_address = db.Column(db.String(100), nullable=False)  # Change from 'ip' to 'ip_address'
+    ip_address = db.Column(db.String(100), nullable=False)  # 'ip' changed to 'ip_address'
     port = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(50), nullable=False)
     type = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return f'<Proxy {self.ip_address}:{self.port}>'
-
 
 class Scan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
